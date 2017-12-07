d098d3c7f130e7b11182fec115e64b03d181cf1f
PC of MinorFish <sztangjd@qq.com>
2017-12-07 12:22:24 +0800

    Upload

--- app/src/main/java/com/tangjd/common/retrofit/string/ApiStringBase.java
+++ app/src/main/java/com/tangjd/common/retrofit/string/ApiStringBase.java
@@ -18,34 +18,30 @@ import retrofit2.Retrofit;
 public class ApiStringBase {
     public static final String TAG = "Api";
 
-    private static Retrofit sStringRetrofit;
-
     public static Retrofit getStringRetrofit(String domainName) {
-        if (sStringRetrofit == null) {
-            //日志显示级别
-            HttpLoggingInterceptor.Level level = HttpLoggingInterceptor.Level.BODY;
-            //新建log拦截器
-            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
-                @Override
-                public void log(String message) {
-                    Log.i(TAG, message);
-                }
-            });
-            OkHttpClient.Builder builder = new OkHttpClient.Builder();
-            builder.retryOnConnectionFailure(false);
-            // Default timeout time
-            builder.connectTimeout(10_000, TimeUnit.MILLISECONDS);
-            builder.readTimeout(30_000, TimeUnit.MILLISECONDS);
-            builder.writeTimeout(10_000, TimeUnit.MILLISECONDS);
-            builder.addInterceptor(loggingInterceptor);
-            OkHttpClient okHttpClient = builder.build();
-            loggingInterceptor.setLevel(level);
-            sStringRetrofit = new Retrofit.Builder()
-                    .baseUrl(domainName)
-                    .addConverterFactory(StringConverterFactory.create())
-                    .client(okHttpClient)
-                    .build();
-        }
+        //日志显示级别
+        HttpLoggingInterceptor.Level level = HttpLoggingInterceptor.Level.BODY;
+        //新建log拦截器
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            @Override
+            public void log(String message) {
+                Log.i(TAG, message);
+            }
+        });
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.retryOnConnectionFailure(false);
+        // Default timeout time
+        builder.connectTimeout(10_000, TimeUnit.MILLISECONDS);
+        builder.readTimeout(30_000, TimeUnit.MILLISECONDS);
+        builder.writeTimeout(10_000, TimeUnit.MILLISECONDS);
+        builder.addInterceptor(loggingInterceptor);
+        OkHttpClient okHttpClient = builder.build();
+        loggingInterceptor.setLevel(level);
+        Retrofit sStringRetrofit = new Retrofit.Builder()
+                .baseUrl(domainName)
+                .addConverterFactory(StringConverterFactory.create())
+                .client(okHttpClient)
+                .build();
         return sStringRetrofit;
     }
 
